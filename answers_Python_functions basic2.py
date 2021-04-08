0f767f3f749781d002a9699c12d172330108d068
Richard Lopez <78048220+Richard-Lopez1@users.noreply.github.com>
2021-04-07 19:02:17 -0700

    Functions Basic 2 Python answers

---
+++ answers_Python_functions basic2.py
@@ -0,0 +1,52 @@
+# FOR SOLUTIONS JUST UN COMMENT EACH SECTION (LISTED BY NUMBERS).
+
+#  (1)answers below are for Functions basic 2 (1)countdown
+# def countdown(x):
+#     answer = []
+#     for val in range(x,-1,-1):
+#         answer.append(val)
+#     return answer
+# print(countdown(5)) 
+
+# (2) answers below are for Functions basic 2 (2) print and return
+# def print_and_return(answer):
+#     print(answer[0])
+#     return answer[1]
+# print(print_and_return([1,2]))
+
+# (3) First plus length
+# def first_plus_length(answer):
+#     return answer[0] + len(answer)
+# print(first_plus_length([1,2,3,4,5]))
+
+# (4) Values greater than secondd
+# def values_greater_2nd(answer):
+#     var = []
+#     # gets the second variable the list
+#     second_number = answer[1] 
+#     for index in range(len(answer)):
+#         if answer[index] > second_number:
+#             var.append(answer[index])
+
+#     print(len(var))
+#     return var
+# print(values_greater_2nd([5,2,3,2,1,4]))
+
+#  (5) This length, that value
+# def lengthvalue(size, value):
+#     answer = []
+#     for x in range(size):
+#         answer.append(value)
+#     return x
+
+# result = lengthvalue(4,7)
+# print(result)
+# print(lengthvalue(result))
+
+# def lengthandvalue(size, value):
+#     answer = []
+#     for x in range(size):
+#         answer.append(value)
+#     return answer 
+# result = lengthandvalue(4,7)
+# print(result)
